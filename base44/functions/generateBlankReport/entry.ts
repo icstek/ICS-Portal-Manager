a81a22383d4a0c91a9434f9904bdd470efabd0b0
udimenashe <udimenashe@gmail.com>
2026-03-26 22:55:47 +0000

    File changes

--- base44/functions/generateBlankReport/entry.ts
+++ base44/functions/generateBlankReport/entry.ts
@@ -164,13 +164,13 @@ Deno.serve(async (req) => {
     yPos = chargeY + 8;
     pdf.setFontSize(7);
     pdf.setFont(undefined, 'bold');
-    pdf.text('Terms of Service:', margin, yPos);
+    pdf.text('Terms & Conditions:', margin, yPos);
     yPos += 3;
     pdf.setFont(undefined, 'normal');
-    pdf.setFontSize(6);
-    const tosText = "Payment is due upon completion of service. We accept cash, check, and credit cards. All equipment left for repair after 30 days will be considered abandoned and will be disposed of or donated at the discretion of the company.";
+    pdf.setFontSize(5.5);
+    const tosText = "The service and repair estimates indicated herein are hereby acceptable to the undersigned. Items not picked up within 30 calendar days from the date below will be subject to sale in order to recover ICS expenses. Customer understands that ICS is not responsible for loss or damage to any equipment in case of fire, theft, or any other causes beyond ICS control. In addition, ICS is not responsible for loss of Customer's programs or data for any reason. Customer is solely responsible to make backup of computer system data, software and applications prior to ICS services herein. All Spyware and Virus Cleanups do not carry warranty for labor due to the nature of the system use with the internet. All returned checks will be charged a $25.00 fee. All returned Sales are subject to a 20% restocking fee. I hereby authorize the repair work herein set forth, to be done with all necessary materials and grants ICS and its employees permission to operate the computer system and other equipment herein described for the purposes of repair and testing at my sole and exclusive risk. An express mechanic's lien is hereby acknowledged on the above equipment to secure the amount of repairs and parts listed in, or hereafter added in, this invoice.";
     pdf.text(tosText, margin, yPos, { maxWidth: contentWidth, align: 'left' });
-    yPos += 8;
+    yPos += 10;
 
     // SIGNATURE SECTION
     yPos += 3;
